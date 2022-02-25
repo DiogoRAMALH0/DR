26a69f8da8033ffa6504053f6fff94315af6e9b0
Diogo <diogo.ramalho.simplon@gmail.com>
2022-02-25 11:50:51 +0100

    update

--- getProfil.php
+++ getProfil.php
@@ -1,7 +1,11 @@
 <?php
-/*
-*/
-$response = file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-b28b4728-5bfc-425c-9dc0-d729cb403bb1");
+
+	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-ce47c040-0b72-42a2-bc42-e4193d453478");
+
+if ($response === FALSE){
+	$response = '{"id": "none","accountId": "none","puuid": "none","name": "inconnu","profileIconId": 0,"revisionDate": 0, "summonerLevel": 0}';
+}
 
 echo $response;
+
 ?>
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 $.get("./getProfil.php?name=technet")
 .done( function(data){
-	profil = jQuery.parseJSON(data);
-	$( "#pseudo" ).text(profil.name);
-	$("#icon").attr("src","http://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png")
-	console.log(profil);
-	console.log(profil.name);
+profil = jQuery.parseJSON(data);
+$( "#pseudo" ).text(profil.name);
+$("#icon").attr("src","http://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png")
+
+});
+
+
+$("#go").click(function(){
+	pseudo = $("#joueur").val();
+	console.log("pseudo : "+ pseudo);
+	console.log("profil");
 });
