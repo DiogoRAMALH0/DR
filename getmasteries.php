1b0f9f1cca79ae25f84c7551fc376982a20f1d73
Diogo <diogo.ramalho.simplon@gmail.com>
2022-02-28 14:51:25 +0100

    update

--- getmasteries.php
+++ getmasteries.php
@@ -1,8 +1,8 @@
 <?php
 
-	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/".$_GET["id"]."?api_key=RGAPI-f20459c0-0e21-40cf-ba21-586d1160615d");
+	$response = file_get_contents("https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/".$_GET["id"]."?api_key=RGAPI-f20459c0-0e21-40cf-ba21-586d1160615d");
 
-if ($response === FALSE){
+/*if ($response === FALSE){
 	$response = '"championId": 0,"championLevel": 0,"championPoints": 0,"lastPlayTime": 0,"championPointsSinceLastLevel": 0,"championPointsUntilNextLevel": 0,"chestGranted": false,"tokensEarned": 0,"summonerId": "0"';
 }*/
 
